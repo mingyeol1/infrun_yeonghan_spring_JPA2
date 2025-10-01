e71e1ddcbaeba27c576f4f6a7bdd47fe8034ddf3
MinGyeol <alsruf741@naver.com>
2025-10-01 16:33:28 +0900

    페이징 과한계돌파

--- src/main/java/japbook/jpashop/api/OrderApiController.java
+++ src/main/java/japbook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -46,6 +47,22 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3(){
      List<Order> orders = orderRepository.findAllWithItem();
+
+        for (Order order : orders){
+            System.out.println("ORDERS REF= " + order + " id = "+ order.getId());
+        }
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit){      //페이지 네이션이 가능한 쿼리
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
         for (Order order : orders){
             System.out.println("ORDERS REF= " + order + " id = "+ order.getId());
