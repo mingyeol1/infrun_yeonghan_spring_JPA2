d7da289525a7acda7b89b4573b908fcf651ae7e3
MinGyeol <alsruf741@naver.com>
2025-10-02 15:28:10 +0900

    DTO 직접조회 컬렉션 조회 최적화

--- src/main/java/japbook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/japbook/jpashop/api/OrderSimpleApiController.java
@@ -56,6 +56,8 @@ public class OrderSimpleApiController {
        return orderSimpleQueryRepository.findOrderDtos();
     }
 
+
+
     @Data
     public class SimpleOrderDto {
         private Long orderId;
